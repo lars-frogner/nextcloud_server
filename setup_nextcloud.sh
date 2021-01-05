88be71c1aa4d40a49696fe8425612fefe82de51e
Lars Frogner <lars_frogner@hotmail.com>
2021-01-05 23:59:29 +0100

    Add configuration of apps

--- setup_nextcloud.sh
+++ setup_nextcloud.sh
@@ -20,11 +20,57 @@ if [ ! -z "$DOMAIN_NAME" ]
     sudo -u www-data php $NEXTCLOUD_DIR/occ config:system:set trusted_domains 3 --value=$DOMAIN_NAME
 fi
 
+# Disable unwanted default apps
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:disable survey_client
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:disable dashboard
+
 # Enable External storages app
 sudo -u www-data php $NEXTCLOUD_DIR/occ app:enable files_external
 
+# Enable Default Encryption Module app
+#sudo -u www-data php $NEXTCLOUD_DIR/occ app:enable encryption
+
 # Install two-factor authentication app
 sudo -u www-data php $NEXTCLOUD_DIR/occ app:install twofactor_totp
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install twofactor_admin
+
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install ransomware_protection
+
+# Install Duplicate Finder
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install duplicatefinder
+
+# Install Breeze Dark theme
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install breezedark
+
+# Install Camera RAW Previews
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install camerarawpreviews
+
+# Install integration apps
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install integration_dropbox
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install integration_google
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install integration_onedrive
+
+# Install disk quota warning app
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install quota_warning
+
+# Install media apps
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install extract
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install audioplayer
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install epubreader
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install notes
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install pdfdraw
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install files_readmemd
+
+# Install and configure Preview Generator
+sudo -u www-data php $NEXTCLOUD_DIR/occ app:install previewgenerator
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:app:set previewgenerator squareSizes --value="32 256"
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:app:set previewgenerator widthSizes  --value="256 384"
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:app:set previewgenerator heightSizes --value="256"
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:system:set preview_max_x --value 2048
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:system:set preview_max_y --value 2048
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:system:set jpeg_quality --value 60
+sudo -u www-data php $NEXTCLOUD_DIR/occ config:app:set preview jpeg_quality --value="60"
+(sudo crontab -u www-data -l; echo "*/15 * * * * sudo -u www-data php $NEXTCLOUD_DIR/occ preview:pre-generate -q" ) | sudo crontab -u www-data -
 
 # Update config.php to get prettier URLs
 sudo sed -i "s/'overwrite.cli.url' => 'http:\/\/localhost',/'overwrite.cli.url' => 'https:\/\/$DOMAIN_NAME\/nextcloud',/g" $NEXTCLOUD_DIR/config/config.php
