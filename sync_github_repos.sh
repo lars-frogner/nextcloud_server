c06c8f94341c019e1cb2de7afdb6da3b7c37506c
Lars Frogner <lars_frogner@hotmail.com>
2021-01-16 23:28:21 +0100

    Allow script to run from anywhere

--- sync_github_repos.sh
+++ sync_github_repos.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Add to crontab to automatically run at midnight:
+# (sudo crontab -l; echo "0 0 * * * <script dir>/sync_github_repos.sh" ) | sudo crontab -
+
+# Directory where repos should be synced to
+LOCAL_REPO_DIR=/mnt/hdd1/lars/files/Kode/github_repos_backup
+
+# Directory of this script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)
+
+# Source GITHUB_USERNAME, GITHUB_PUBLIC_KEY and GITHUB_API_TOKEN variables
+source $SCRIPT_DIR/github_sync_config.sh
+
+cd $LOCAL_REPO_DIR
+
 # Get list of all GitHub repositories
 REPOS=$(curl -s -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/search/repositories?q=user:$GITHUB_USERNAME | grep -o 'git@[^"]*')
 
