b0eb9ad79bbf2b44c6b244d8a91d515a233bcc01
Lars Frogner <lars_frogner@hotmail.com>
2021-01-23 22:43:46 +0100

    Specify Nextcloud version to download

--- install_nextcloud.sh
+++ install_nextcloud.sh
@@ -4,10 +4,12 @@ set -x
 
 export NEXTCLOUD_ROOT=/var/www
 
+ARCHIVE_NAME=nextcloud-20.0.4.tar.bz2
+
 # Download and unpack latest nextcloud version in /var/www/
-sudo wget https://download.nextcloud.com/server/releases/latest.tar.bz2 -P $NEXTCLOUD_ROOT/
-sudo tar -xvf $NEXTCLOUD_ROOT/latest.tar.bz2 -C $NEXTCLOUD_ROOT/
-sudo rm $NEXTCLOUD_ROOT/latest.tar.bz2
+sudo wget https://download.nextcloud.com/server/releases/$ARCHIVE_NAME -P $NEXTCLOUD_ROOT/
+sudo tar -xvf $NEXTCLOUD_ROOT/$ARCHIVE_NAME -C $NEXTCLOUD_ROOT/
+sudo rm $NEXTCLOUD_ROOT/$ARCHIVE_NAME
 sudo chown -R www-data:www-data $NEXTCLOUD_ROOT/nextcloud/ # Set owner
 
 set +x
