e450e119f6097e7527f56a22eaacfabe7655903c
Lars Frogner <lars_frogner@hotmail.com>
2021-01-23 22:45:04 +0100

    Add flag for whether to setup static IP with dhcpcd

--- setup_network.sh
+++ setup_network.sh
@@ -4,23 +4,27 @@ set -x
 
 CONNECTION=$(ip route get 8.8.8.8 | grep -Po 'dev \K\w+' | grep -qFf - /proc/net/wireless && echo wlan0 || echo eth0)
 export HOST_IP=$(ip -o -4 addr list $CONNECTION | awk '{print $4}' | cut -d/ -f1)
-ROUTER_IP=$(ip r | grep default | sed -n "s/^default via \([0-9\.]*\).*$/\1/p")
-DNS_IP=$(sudo sed -n "s/^nameserver \([0-9\.]*\).*$/\1/p" /etc/resolv.conf | head -n 1)
 
-# Check if dhcpcd service is running
-if [[ -z $(systemctl | grep dhcpcd.service) ]]
-then
-    # If not, start it
-    sudo service dhcpcd start
-    sudo systemctl enable dhcpcd
+SETUP_PRIVATE_IP=false
+if [[ "$SETUP_PRIVATE_IP" = true ]]; then
+    ROUTER_IP=$(ip r | grep default | sed -n "s/^default via \([0-9\.]*\).*$/\1/p")
+    DNS_IP=$(sudo sed -n "s/^nameserver \([0-9\.]*\).*$/\1/p" /etc/resolv.conf | head -n 1)
+
+    # Check if dhcpcd service is running
+    if [[ -z $(systemctl | grep dhcpcd.service) ]]
+    then
+        # If not, start it
+        sudo service dhcpcd start
+        sudo systemctl enable dhcpcd
+    fi
+
+    # Set static IP to current IP in dhcpcd.conf
+    echo "
+    interface $CONNECTION
+    static ip_address=$HOST_IP/24
+    static routers=$ROUTER_IP
+    static domain_name_servers=$DNS_IP" | sudo tee -a /etc/dhcpcd.conf
 fi
-
-# Set static IP to current IP in dhcpcd.conf
-echo "
-interface $CONNECTION
-static ip_address=$HOST_IP/24
-static routers=$ROUTER_IP
-static domain_name_servers=$DNS_IP" | sudo tee -a /etc/dhcpcd.conf
 
 # Disable bluetooth
 echo "
@@ -28,8 +32,8 @@ echo "
 # Disable Bluetooth
 dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
 
+set +e
 sudo systemctl disable hciuart.service
-sudo systemctl disable bluealsa.service
 sudo systemctl disable bluetooth.service
 
 set +x
